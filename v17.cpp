9d582e072e34dfb66bbd08aa8d47c7bd4147a26b
Komronbek-Urinboev <komronbekurinboev06@gmail.com>
2026-05-22 12:08:35 +0500

    Add functionality to count letters in Russian names
    Replaced string variables with Russian names and added functionality to count letters in names.

--- v17.cpp
+++ v17.cpp
@@ -1,36 +1,15 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main() {
-    string s1 = "triangle";
-    string s2 = "integral";
-
-    int counts1[256] = {0};
-    int counts2[256] = {0};
-
-    for (int i = 0; i < s1.length(); i++) {
-        counts1[(unsigned char)s1[i]]++;
-    }
-
-    for (int i = 0; i < s2.length(); i++) {
-        counts2[(unsigned char)s2[i]]++;
-    }
-
-    bool is_anagram = true;
-    for (int i = 0; i < 256; i++) {
-        if (counts1[i] != counts2[i]) {
-            is_anagram = false;
-            break;
-        }
-    }
-
-    if (is_anagram && s1.length() == s2.length()) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
-    }
+    std::string firstName = "Иван";
+    std::string lastName = "Иванов";
+    setlocale(LC_ALL, "Russian");
+    int firstNameLetters = firstName.length() / 2; 
+    int lastNameLetters = lastName.length() / 2;
+    std::cout << "Букв в имени: " << firstNameLetters << std::endl;
+    std::cout << "Букв в фамилии: " << lastNameLetters << std::endl;
+    std::cout << "Всего букв: " << firstNameLetters + lastNameLetters << std::endl;
 
     return 0;
 }
